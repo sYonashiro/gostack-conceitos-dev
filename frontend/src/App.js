eaddbbad81d7cb0f4b8931a4a1c60cebaf7a44cf
Sergio Yonashiro <sergio.yonashiro@outlook.com>
2020-08-25 00:37:07 -0300

    Renderiza conteúdo do componente através da propriedade "children"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,8 +5,19 @@ import Header from './components/Header';
 function App() {
   return (
     <>
-      <Header title="Homepage" />
-      <Header title="Projects" />
+      <Header title="Homepage">
+        <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+        </ul>
+      </Header>
+      <Header title="Projects">
+      <ul>
+          <li>Homepage</li>
+          <li>Projects</li>
+          <li>Login</li>
+        </ul>
+      </Header>
     </>
   );
 }
