feae8e4702cb7f7e542505e80e9c058f97dc899b
Sergio Yonashiro <sergio.yonashiro@outlook.com>
2020-08-27 23:05:47 -0300

    Aplica estilos e imagens na página utilizando seus respectivos loaders do Webpack

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React, {useState} from 'react';
 
+import './App.css';
+import backgroundImage from './assets/background.jpg';
+
 import Header from './components/Header';
 
 function App() {
@@ -12,6 +15,8 @@ function App() {
   return (
     <>
       <Header title="Projects" />
+
+      <img width={300} src={backgroundImage} />
 
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
